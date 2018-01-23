03f4624924790b6da80b7f3a99deb22273ca7694
宮部敏史 <satoshi@air.local>
2018-01-23 18:20:00 +0900

    エラーになるので削除

--- emacs.rb
+++ emacs.rb
@@ -8,7 +8,6 @@ class Emacs < Formula
   conflicts_with "emacs"
 
   bottle do
-    revision 2
     sha256 "2442a949678d9b3cbe99e9b504917a641de57258d2a40dc85e8a70efae82bb38" => :el_capitan
     sha256 "751b8b481b30870273243eae77ea08eb2b0b5a2fbcbc62453b7cf7632ac69445" => :yosemite
     sha256 "3889a7cbda704f604b3a6187c8683ea1e6e4e600e1e7a0b8b59f33533e8f3023" => :mavericks
